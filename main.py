4dc72f9d7e2d3422031582d41f24984f80593410
itsnotrin <30768410+itsnotrin@users.noreply.github.com>
2021-12-07 21:36:22 +0000

    broken commit, json decode error. Going to sleep

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from requests.sessions import session
 from sanic import Sanic
 from sanic.response import json
 import requests
@@ -14,12 +15,23 @@ def login(code, dob):
     session = requests.Session()
     resp = session.post(login_url, headers=headers, data=payload)
     jsonResponse = resp.json()
+    studentId = jsonResponse["data"]["id"]
+    sessionId = jsonResponse["meta"]["session_id"]
+    headers = {
+        "authorization": f"Basic {sessionId}"
+    }
+    resp2 = session.get(f"https://www.classcharts.com/apiv2student/{studentId}", headers = headers)
+    homeworks = []
+    jsonResponse2 = resp2.json()
+    print(jsonResponse2)
     if jsonResponse["success"] == 0:
         print("Error while logging in - Your date of birth or your login code is incorrect!")
-        return(0, "ERROR - DOB OR CODE", session)
+        return(0, "ERROR - DOB OR CODE", jsonResponse, session)
     else:
         name = jsonResponse["data"]["name"]
-        return(1, name, jsonResponse)
+        return(1, name, jsonResponse, session)
+
+
 
 app = Sanic("The backend for my BetterClasscharts Project!")
 
@@ -28,12 +40,14 @@ def signin(request):
     req = request.json
     code = req["code"]
     dob = req["dob"]
-    success, name, session = login(code, dob)
-    if success == 1:
-        print(session)
-        return json({ "success": 1, "message": f"Welcome, {name}!" })
-    else:
+    success, name, resp, session = login(code, dob)
+    if success == 0:
         return json({ "success": 0, "message": "Your Date of birth or your login code is incorrect. Please try again!"})
+
+    else:
+        student_id = resp["data"]["id"]
+        return json({})
+
 
 
 if __name__ == '__main__':
