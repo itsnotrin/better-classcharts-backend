6913568dbabbe3ff3d7693d14d5df07d9d623fc8
itsnotrin <30768410+itsnotrin@users.noreply.github.com>
2021-12-07 21:12:04 +0000

    Updated the login endpoint, fully working now!

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from sanic import Sanic
 from sanic.response import json
 import requests
+from datetime import datetime, timedelta
 
 # Storing key info
-homework_url = "https://www.classcharts.com/apipublic/homework"
+homework_url = "https://www.classcharts.com/apipublic/homework/0"
 login_url = "https://www.classcharts.com/apiv2student/login"
 
 # Login System: 
@@ -15,16 +16,25 @@ def login(code, dob):
     jsonResponse = resp.json()
     if jsonResponse["success"] == 0:
         print("Error while logging in - Your date of birth or your login code is incorrect!")
-        return(1, "ERROR - DOB OR CODE", session)
+        return(0, "ERROR - DOB OR CODE", session)
     else:
         name = jsonResponse["data"]["name"]
-        return(0, name, session)
+        return(1, name, jsonResponse)
 
 app = Sanic("The backend for my BetterClasscharts Project!")
 
 @app.route("/login")
-def post_json(request):
-    return json({ "received": True, "message": request.json })
+def signin(request):
+    req = request.json
+    code = req["code"]
+    dob = req["dob"]
+    success, name, session = login(code, dob)
+    if success == 1:
+        print(session)
+        return json({ "success": 1, "message": f"Welcome, {name}!" })
+    else:
+        return json({ "success": 0, "message": "Your Date of birth or your login code is incorrect. Please try again!"})
+
 
 if __name__ == '__main__':
     app.run()
