8f52794da8827d8a5d3d89c9b11e340e090fa3fe
itsnotrin <30768410+itsnotrin@users.noreply.github.com>
2021-12-08 15:15:32 +0000

    Fix the debugging

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ def login(code, dob):
     })
     homeworks = []
     jsonResponse2 = resp2.json()
-    print(jsonResponse2["data"][0] + '\n\n\n\n')
-    print(f"Class: {jsonResponse2['data'][2]['lesson']}\nLesson: {jsonResponse2['data'][2]['subject']}")
+    print(jsonResponse2["data"][0])
+    print(f"\n\n\nClass: {jsonResponse2['data'][2]['lesson']}\nLesson: {jsonResponse2['data'][2]['subject']}")
     if jsonResponse["success"] == 0:
         print("Error while logging in - Your date of birth or your login code is incorrect!")
         return(0, "ERROR - DOB OR CODE", jsonResponse, session)
