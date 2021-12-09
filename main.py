72e2c2a08576fc04a00794da557607784df34b7c
itsnotrin <30768410+itsnotrin@users.noreply.github.com>
2021-12-09 15:15:46 +0000

    Began work on timetable

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from sanic.response import json
 import requests
 
 # Storing key info
-homework_url = "https://www.classcharts.com/apipublic/homework/0"
 login_url = "https://www.classcharts.com/apiv2student/login"
 
 # Homework System: 
@@ -15,9 +14,6 @@ def GetHomework(code, dob):
     jsonResponse = resp.json()
     studentId = jsonResponse["data"]["id"]
     sessionId = jsonResponse["meta"]["session_id"]
-    headers = {
-        "authorization": f"Basic {sessionId}"
-    }
     resp2 = session.get(f"https://www.classcharts.com/apiv2student/homeworks/{studentId}?", headers = {
       "authorization": f"Basic {sessionId}"
     })
@@ -43,6 +39,7 @@ def GetHomework(code, dob):
         name = jsonResponse["data"]["name"]
         return(1, name, homeworks)
 
+
 # Behaviour System: 
 def GetBehaviour(code, dob):
     headers = {"User-Agent": "Mozilla/5.0"}
@@ -55,7 +52,7 @@ def GetBehaviour(code, dob):
     headers = {
         "authorization": f"Basic {sessionId}"
     }
-    #https://www.classcharts.com/apiv2student/activity/5939358?from=2021-08-01&to=2021-12-09
+    #TODO: ALLOW USERS TO SET A SPECIFIC TIME FRAME TO REPORT BACK FROM.
     resp2 = session.get(f"https://www.classcharts.com/apiv2student/activity/{studentId}", headers = {
       "authorization": f"Basic {sessionId}"
     })
@@ -72,7 +69,7 @@ def GetBehaviour(code, dob):
       elif i["polarity"] == "positive":
         NegCount+=1
       data = {
-          Count: {
+          int(Count): {
             "type": i["polarity"],
             "teacher": i["teacher_name"],
             "note": i["note"]
@@ -88,13 +85,37 @@ def GetBehaviour(code, dob):
         name = jsonResponse["data"]["name"]
         return(1, name, points)
 
+# Timetable System: 
+def GetTimetable(code, dob):
+    headers = {"User-Agent": "Mozilla/5.0"}
+    payload = {'code': code, 'dob':dob, 'remember_me': '1'}
+    session = requests.Session()
+    resp = session.post(login_url, headers=headers, data=payload)
+    jsonResponse = resp.json()
+    studentId = jsonResponse["data"]["id"]
+    sessionId = jsonResponse["meta"]["session_id"]
+    resp2 = session.get(f"https://www.classcharts.com/apiv2student/timetable/{studentId}", headers = {
+      "authorization": f"Basic {sessionId}"
+    })
+    jsonResponse2 = resp2.json()
+    print(jsonResponse2["data"])
+    if jsonResponse["success"] == 0:
+        print("Error while logging in - Your date of birth or your login code is incorrect!")
+        return(0, "ERROR - DOB OR CODE", jsonResponse, session)
+    else:
+        name = jsonResponse["data"]["name"]
+        return(1, name, jsonResponse2)
+
 app = Sanic("The backend for my BetterClasscharts Project!")
 
 @app.route("/homework")
 def HomeworkEndpoint(request):
     req = request.json
-    code = req["code"]
-    dob = req["dob"]
+    try:
+      code = req["code"]
+      dob = req["dob"]
+    except:
+      return json({ "success": 0, "message": "Code or Date of Birth is missing." })
     success, name, homeworks = GetHomework(code, dob)
     if success == 0:
         return json({ "success": 0, "message": "Your Date of birth or your login code is incorrect. Please try again!"})
@@ -105,8 +126,11 @@ def HomeworkEndpoint(request):
 @app.route("/behaviour")
 def BehaviourEndpoint(request):
     req = request.json
-    code = req["code"]
-    dob = req["dob"]
+    try:
+      code = req["code"]
+      dob = req["dob"]
+    except:
+      return json({ "success": 0, "message": "Code or Date of Birth is missing." })
     success, name, points = GetBehaviour(code, dob)
     if success == 0:
         return json({ "success": 0, "message": "Your Date of birth or your login code is incorrect. Please try again!"})
@@ -114,6 +138,22 @@ def BehaviourEndpoint(request):
     else:
         return json({ "success": 1, "message": points })
 
+@app.route("/timetable")
+def TimetableEndpoint(request):
+    req = request.json
+    try:
+      code = req["code"]
+      dob = req["dob"]
+    except:
+      return json({ "success": 0, "message": "Code or Date of Birth is missing." })
+    GetTimetable(code, dob)
+    return json({})
+    # success, name, points = GetBehaviour(code, dob)
+    # if success == 0:
+    #     return json({ "success": 0, "message": "Your Date of birth or your login code is incorrect. Please try again!"})
+
+    # else:
+    #     return json({ "success": 1, "message": points })
 
 if __name__ == '__main__':
     app.run("0.0.0.0")
